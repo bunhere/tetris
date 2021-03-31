c48a8b283070b5ed93af7aa8cbd221b8e108678d
Ryuan Choi <ryuan.choi@navercorp.com>
2021-03-31 11:26:46 +0900

    Attach MainView as canvas

---
+++ README.md
@@ -0,0 +1,10 @@
+1. 기본 화면 만들기(11:08~)
+
+- activity_main.xml 에서 TextView 제거(어차피 아래서 제거됨)
+- canvas 만들기
+  - android java canvas tutorial 를 검색해서 링크들을 확인
+  - https://www.javatpoint.com/android-simple-graphics-example 를 보니 DemoView를 넣는 방법이 제일 간단해 보임
+  - MainActivity.java 에 비슷하게 코딩. 내가 만드는건 Domo가 아니니까 MainView라고 하자.
+  - java coding convention은 기억이 안나는데 개인코드니까 내맘대로 mHelloWorld 형태로 
+  - 그나저나, 이렇게 되면 activity_main.xml이 아예 필요 없겠네;;
+
--- app/src/main/java/com/ryuan/tetris/MainActivity.java
+++ app/src/main/java/com/ryuan/tetris/MainActivity.java
@@ -2,13 +2,30 @@ package com.ryuan.tetris;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.graphics.Canvas;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    MainView mMainView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        mMainView = new MainView(this);
+        setContentView(mMainView);
+    }
+
+    private class MainView extends View {
+        public MainView(Context context) {
+            super(context);
+        }
+
+        @Override
+        protected void onDraw(Canvas canvas) {
+            super.onDraw(canvas);
+        }
     }
 }
